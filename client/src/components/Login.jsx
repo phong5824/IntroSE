94d656c5aa180ddbae039acfc66734fd66182007
Phong ne <phog.27.12@gmail.com>
2023-11-03 16:43:36 +0700

    Login, Register

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { handleLogin } from "../action/accountAction";
+import Logo from '../assets/logo-recipe.png';
 
 export default function Login() {
   const [email, setEmail] = useState("");
@@ -13,8 +14,8 @@ export default function Login() {
       email: email,
       password: password,
     };
-    if (email === ""|| password === "") {
-      alert("Please fill in all fields");
+    if (email === "" || password === "") {
+      alert("Vui lòng điền đầy đủ thông tin");
       return;
     }
     if (handleLogin(userData)) {
@@ -22,53 +23,86 @@ export default function Login() {
     }
   };
 
-  return (
+  const containerStyle = {
+    backgroundColor: "#7CFFC0",
+    width: "500px",
+    height: "500px",
+    borderRadius: "20px",
+  };
 
-  <div className="flex items-center justify-center h-screen">
-  <div className="login-container flex relative flex-col items-center justify-around w-[400px] h-[390px] bg-green-200 border border-blue-700 rounded-lg shadow-lg">
-    <div
-      className="login-logo w-10 h-10 rounded-full bg-cyan-300"
-      style={{
-        backgroundImage:
-          "url('https://i.pinimg.com/originals/8b/44/51/8b4451665d6b2139e29f29b51ffb1829.png')",
-        backgroundRepeat: "no-repeat",
-        backgroundSize: "cover",
-      }}
-    ></div>
-    <div className="login-form w-[300px]" >
-      <form className="flex flex-col justify-around w-full" onSubmit={onSubmit}>
-        <label htmlFor="email" className="text-blue-700 mb-1 w-full font-bold">Email</label>
-        <input
-          type="text"
-          name="email"
-          onChange={(e) => {
-            setEmail(e.target.value);
+  const labelStyle = {
+    backgroundColor: "#7CFFC0",
+    fontFamily: 'Poppins, sans-serif',
+    color: "black",
+    fontSize: "18px",
+    fontWeight: "bold",
+    textAlign: "left",
+    padding: "10px 20px",
+    margin: "0 auto",
+  };
+
+  const buttonStyle = {
+    backgroundColor: "#FFADB7",
+    fontFamily: 'Poppins, sans-serif',
+    color: "black",
+    fontSize: "15px",
+    textAlign: "center",
+    padding: "10px 20px",
+    borderRadius: "20px",
+    cursor: "pointer",
+    margin: "0 auto",
+  };
+
+  const pageStyle = {
+    backgroundColor: "#7CFF99",
+  };
+
+  return (
+    <div style={pageStyle} className="flex items-center justify-center h-screen">
+      <div className="login-container flex relative flex-col items-center justify-around w-[500px] h-[450px]" style={containerStyle}>
+        <div
+          className="login-logo w-24 h-24 rounded-full mb-[-4rem]"
+          style={{
+            backgroundImage: `url(${Logo})`,
+            backgroundRepeat: "no-repeat",
+            backgroundSize: "cover",
           }}
-          className="border border-orange-300 px-3 py-2 rounded-md focus:outline-none focus:ring-2 focus:ring-cyan-300 w-full mb-3"
-          style={{ backgroundColor: "lightblue" }}
-        />
-        <label htmlFor="password" className="text-blue-700 mb-1 font-bold">Password</label>
-        <input
-          type="password"
-          name="password"
-          onChange={(e) => {
-            setPassword(e.target.value);
-          }}
-          className="border border-orange-300 px-3 py-2 rounded-md focus:outline-none focus:ring-2 focus:ring-cyan-300 w-full mb-3
-          font-bold 
-          "
-          style={{ backgroundColor: "lightblue" }}
-        />
-        <button type="submit" className="btn bg-gray-900 w-full text-white py-2 rounded-md">
-          Login
-        </button>
-        <Link to="/register" className="btn text-gray-700 hover:text-gray-900 mt-2">
-          Register
-        </Link>
-      </form>
-    </div>
-  </div>
-</div>
-          
+        ></div>
+        <div className="login-form w-[350px] py-1">
+          <form className="flex flex-col justify-around w-full" onSubmit={onSubmit}>
+            <div style={labelStyle}>
+              Tên đăng nhập
+            </div>
+            <input
+              type="text"
+              name="email"
+              onChange={(e) => {
+                setEmail(e.target.value);
+              }}
+              className="border px-3 py-2 rounded-5 focus:outline-none focus:ring-2 focus:ring-cyan-300 w-[100%] bg-white text-center"
+              placeholder="Nhập ở đây..."
+            />
+            <div style={labelStyle}>
+              Mật khẩu
+            </div>
+            <input
+              type="password"
+              name="password"
+              onChange={(e) => {
+                setPassword(e.target.value);
+              }}
+              className="border px-3 py-2 rounded-5 focus:outline-none focus:ring-2 focus:ring-cyan-300 w-full text-center"
+              placeholder="Nhập ở đây..."
+            />
+            <button className="btn w-[50%] py-2 rounded-md mt-3" style={buttonStyle} >
+              Đăng nhập
+            </button>
+            <Link to="/register" className="btn text-gray-700 hover:text-gray-900 mt-3" style={{ labelStyle, margin: "0 auto" }}>
+              Đăng ký tài khoản
+            </Link>
+          </form>
+        </div>
+      </div>
+    </div >
   );
 }
--- client/src/components/Register.jsx
+++ client/src/components/Register.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { handleRegister } from "../action/accountAction";
 import { useNavigate } from "react-router-dom";
+import Logo from '../assets/logo-recipe.png';
 
 export default function Register() {
   const [email, setEmail] = useState("");
@@ -14,7 +15,7 @@ export default function Register() {
       password: password,
     };
     if (email === "" || password === "") {
-      alert("Please fill in all fields");
+      alert("Vui lòng điền đầy đủ thông tin");
       return;
     }
     if (handleRegister(userData)) {
@@ -22,48 +23,88 @@ export default function Register() {
     }
   };
 
+  const containerStyle = {
+    backgroundColor: "#7CFFC0",
+    width: "500px",
+    height: "500px",
+    borderRadius: "20px",
+  };
+
+  const labelStyle = {
+    backgroundColor: "#7CFFC0",
+    fontFamily: 'Poppins, sans-serif',
+    color: "black",
+    fontSize: "18px",
+    fontWeight: "bold",
+    textAlign: "left",
+    padding: "10px 20px",
+    margin: "0 auto",
+  };
+
+  const buttonStyle = {
+    backgroundColor: "#FFADB7",
+    fontFamily: 'Poppins, sans-serif',
+    color: "black",
+    fontSize: "15px",
+    textAlign: "center",
+    padding: "10px 20px",
+    borderRadius: "20px",
+    cursor: "pointer",
+    margin: "0 auto",
+  };
+
+  const pageStyle = {
+    backgroundColor: "#7CFF99",
+  };
+
   return (
-    <div className="flex items-center justify-center h-screen">
-  <div className="login-container flex relative flex-col items-center justify-around w-[400px] h-[390px] bg-green-200 border border-blue-700 rounded-lg shadow-lg">
-    <div
-      className="login-logo w-10 h-10 rounded-full bg-cyan-300"
-      style={{
-        backgroundImage:
-          "url('https://i.pinimg.com/originals/8b/44/51/8b4451665d6b2139e29f29b51ffb1829.png')",
-        backgroundRepeat: "no-repeat",
-        backgroundSize: "cover",
-      }}
-    ></div>
-    <div className="login-form w-[300px]" >
-      <form className="flex flex-col justify-around w-full" onSubmit={onSubmit}>
-        <label htmlFor="email" className="text-blue-700 mb-1 w-full font-bold">Email</label>
-        <input
-          type="text"
-          name="email"
-          onChange={(e) => {
-            setEmail(e.target.value);
+    <div style={pageStyle} className="flex items-center justify-center h-screen">
+      <div className="login-container flex relative flex-col items-center justify-around" style={containerStyle}>
+        <div
+          className="login-logo w-24 h-24 rounded-full mb-[-4rem]"
+          style={{
+            backgroundImage: `url(${Logo})`,
+            backgroundRepeat: "no-repeat",
+            backgroundSize: "cover",
           }}
-          className="border border-orange-300 px-3 py-2 rounded-md focus:outline-none focus:ring-2 focus:ring-cyan-300 w-full mb-3"
-          style={{ backgroundColor: "lightblue" }}
-        />
-        <label htmlFor="password" className="text-blue-700 mb-1 font-bold">Password</label>
-        <input
-          type="password"
-          name="password"
-          onChange={(e) => {
-            setPassword(e.target.value);
-          }}
-          className="border border-orange-300 px-3 py-2 rounded-md focus:outline-none focus:ring-2 focus:ring-cyan-300 w-full mb-3
-          font-bold 
-          "
-          style={{ backgroundColor: "lightblue" }}
-        />
-        <button type="submit" className="btn bg-gray-900 w-full text-white py-2 rounded-md">
-          Submit
-        </button>
-      </form>
+        ></div>
+        <div className="login-form w-[350px] py-1">
+          <form className="flex flex-col justify-around w-full" onSubmit={onSubmit}>
+            <div style={labelStyle}>Tên đăng nhập</div>
+            <input
+              type="text"
+              name="ten dang nhap"
+              // onChange={(e) => {
+              // }}
+              className="border px-3 py-2 rounded-5 focus:outline-none focus:ring-2 focus:ring-cyan-300 w-[100%] bg-white text-center"
+              placeholder="Nhập ở đây..."
+            />
+            <div style={labelStyle}>Địa chỉ email</div>
+            <input
+              type="text"
+              name="email"
+              onChange={(e) => {
+                setEmail(e.target.value);
+              }}
+              className="border px-3 py-2 rounded-5 focus:outline-none focus:ring-2 focus:ring-cyan-300 w-[100%] bg-white text-center"
+              placeholder="Nhập ở đây..."
+            />
+            <div style={labelStyle}>Mật khẩu</div>
+            <input
+              type="password"
+              name="password"
+              onChange={(e) => {
+                setPassword(e.target.value);
+              }}
+              className="border px-3 py-2 rounded-5 focus:outline-none focus:ring-2 focus:ring-cyan-300 w-full text-center"
+              placeholder="Nhập ở đây..."
+            />
+            <button className="btn w-[50%] py-2 rounded-md mt-4" style={buttonStyle}>
+              Đăng ký
+            </button>
+          </form>
+        </div>
+      </div>
     </div>
-  </div>
-</div>
   );
 }
