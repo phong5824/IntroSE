fb03093318478d252dfd60cca8485dbea0c12007
Pake <100368114+TTH-Pake@users.noreply.github.com>
2023-11-05 00:15:03 +0700

    update account.js

--- server/routes/account.js
+++ server/routes/account.js
@@ -22,7 +22,11 @@ router.post("/login", async(req, res) => {
   
           if (result.password === password && result.password) {
             console.log("Success");
-            res.json({ success: true, message:"Login Success"});
+            
+            const accessToken = jwt.sign({userid:result._id},process.env.ACCESS_TOKEN_SECRET);
+
+            res.status(200).json({ success: true, message:"Login Success",accessToken});
+
           } else {
             console.log("Fail");
             res.status(404)
@@ -40,24 +44,35 @@ router.post("/login", async(req, res) => {
     
   });
   
- router.post("/register", async(req, res) => {
+  router.post("/register", async (req, res) => {
     const { email, password } = req.body;
-    accountModel.findOne({email:email }).then((result) => {
-      if (result) {
-        res
-          .status(409)
-          .json({ success: false, error: "Username already exists!" });
-      } else {
-        console.log("Success");
-        const account = new accountModel({
-          email: email,
-          password: password,
-        });
-        account.save().then(() => {
-          res.json({ success: true, message:"Register Success"});
-        });
-      }
-    });
+    const result = await accountModel.findOne({ email: email });
+  
+    if (result) {
+      res
+        .status(409)
+        .json({ success: false, error: "Username already exists!" });
+    } else {
+      console.log("Success");
+      
+      const maxUser = await accountModel.findOne({}, {}, { sort: { user_id: -1 } });
+      const maxUserId = maxUser ? maxUser.user_id : 0; // Kiểm tra nếu có bản ghi user_id lớn nhất
+      
+      console.log(maxUserId, email, password) 
+      const account = new accountModel({
+        user_id: maxUserId + 1,
+        email: email,
+        password: password,
+      });
+  
+      account.save().then(() => {
+        const accessToken = jwt.sign({userid:account._id},process.env.ACCESS_TOKEN_SECRET);
+        res.json({ success: true, message: "Register Success",accessToken});
+      });
+    }
   });
+  
+
+
 
   module.exports = router;
