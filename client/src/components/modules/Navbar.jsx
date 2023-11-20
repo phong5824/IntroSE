3424211226c10c146149347ca4dcfac428ce6780
Phong ne <phog.27.12@gmail.com>
2023-11-20 19:52:59 +0700

    Merge main

--- client/src/components/modules/Navbar.jsx
+++ client/src/components/modules/Navbar.jsx
@@ -6,20 +6,7 @@ import Avatar from "./Avatar";
 import Menu from "./Menu";
 
 const NavBar = () => {
-<<<<<<< HEAD
-    return (
-        <nav className="navbar flex relative items-center justify-between px-6 h-[80px] bg-green-500">
-            <LogoIcon className="flex-shrink-0" />
-            <SearchBar className="flex-grow mx-2" />
-            <div className="flex-shrink-0 flex items-center space-x-6 bg-green-500">
-                <Avatar />
-                <Menu />
-            </div>
-        </nav>
 
-
-    );
-=======
   const [showLoginForm, setShowLoginForm] = useState(false);
   const [showMenu, setShowMenu] = useState(false);
 
@@ -55,7 +42,6 @@ const NavBar = () => {
       </div>
     </nav>
   );
->>>>>>> main
 };
 
 export default NavBar;
--- client/src/components/modules/RecommendedRecipes.jsx
+++ client/src/components/modules/RecommendedRecipes.jsx
@@ -1,37 +1,3 @@
-<<<<<<< HEAD
-import { useState } from 'react';
-import RecipeCard from "/src/containers/RecipeCard";
-
-const RecommendedRecipes = () => {
-    const [recipes] = useState([
-        { id: 1, name: 'Recipe 1', image: 'recipe1.jpg' },
-        { id: 2, name: 'Recipe 2', image: 'recipe2.jpg' },
-        { id: 3, name: 'Recipe 3', image: 'recipe3.jpg' },
-        { id: 4, name: 'Recipe 4', image: 'recipe4.jpg' },
-        { id: 5, name: 'Recipe 5', image: 'recipe5.jpg' },
-        { id: 6, name: 'Recipe 6', image: 'recipe6.jpg' },
-        { id: 7, name: 'Recipe 7', image: 'recipe7.jpg' },
-        { id: 8, name: 'Recipe 8', image: 'recipe8.jpg' },
-    ]);
-
-    const [isExpanded, setIsExpanded] = useState(false);
-
-    const toggleExpansion = () => {
-        setIsExpanded((prev) => !prev);
-    };
-
-    return (
-        <div className="bg-gray-100 p-4 rounded-lg">
-            <h2 className="text-2xl font-bold mb-4">Recommended Recipes</h2>
-            <div className="grid grid-cols-2 md:grid-cols-4 gap-1">
-                {isExpanded
-                    ? recipes.map((recipe) => (
-                        <RecipeCard key={recipe.id} recipe={recipe} />
-                    ))
-                    : recipes.map((recipe, index) => ( // Sử dụng recipes.length để quyết định hiển thị tất cả
-                        index < 4 && <RecipeCard key={recipe.id} recipe={recipe} />
-                    ))}
-=======
 import { useState } from "react";
 import Image1 from "/src/assets/recipe1.png";
 import starIcon from "/src/assets/star.png";
@@ -93,14 +59,9 @@ const RecommendRecipe = () => {
                         Thêm
                     </button>
                 )}
->>>>>>> main
+
             </div>
-            <button
-                className="bg-green-500 text-white px-4 py-2 rounded-full mt-4 mx-auto block"
-                onClick={toggleExpansion}
-            >
-                {isExpanded ? 'Rút gọn' : 'Mở rộng'}
-            </button>
+
         </div>
     );
 };
