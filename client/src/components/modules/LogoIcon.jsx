9880e6559c70fd7c63f2b60cf694bbbb0f583c6e
Phong ne <phog.27.12@gmail.com>
2023-11-20 17:03:01 +0700

    Add font logo

--- client/src/components/modules/LogoIcon.jsx
+++ client/src/components/modules/LogoIcon.jsx
@@ -1,4 +1,5 @@
 import logoImage from "/src/assets/logo-recipe.png";
+import "/src/fonts.css";
 
 const LogoIcon = () => {
     return (
@@ -6,7 +7,7 @@ const LogoIcon = () => {
             <div className="w-12 h-12 rounded-full overflow-hidden bg-white">
                 <img src={logoImage} alt="Logo" className="w-full h-full object-contain" />
             </div>
-            <span className="font-extrabold text-2xl text-green-900 ">Love Cook</span>
+            <span className="font-lemonada font-bold text-2xl text-gray-800">LoveCook</span>
         </div>
     );
 }
