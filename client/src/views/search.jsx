ed547099cdb6b23e4ba57320df0f2e0a287b8932
AnGaRu <angaru@AnGaRus-MBP.lan>
2023-12-03 18:29:25 +0700

    handle filterIngredient

--- client/src/views/search.jsx
+++ client/src/views/search.jsx
@@ -1,6 +1,7 @@
 /* eslint-disable no-unused-vars */
 import { useEffect, useState } from "react";
 import starIcon from "/src/assets/star.png";
+import trashCan from "/src/assets/trash_can.svg";
 import { handleSearchRecipes } from "./../action/recipesAction";
 import { message } from "antd";
 import { Link, useNavigate, useLocation } from "react-router-dom";
@@ -11,7 +12,9 @@ import loupe from "/src/assets/loupe.png";
 const initialRecipesToShow = 10;
 
 const Search = () => {
+
   const [showLoginForm, setShowLoginForm] = useState(false);
+  const [selectedIngredients, setSelectedIngredients] = useState([]);
 
   const [recipesToShow, setRecipesToShow] = useState(initialRecipesToShow);
   const [name, setName] = useState("");
@@ -19,6 +22,11 @@ const Search = () => {
 
   const location = useLocation();
   const keywords = new URLSearchParams(location.search).get("keywords");
+
+  const [isOpen, setIsOpen] = useState(false);
+
+  const items = ['carrot', 'meat', 'fish', 'Eggs', 'sugar', 'salt', 'walnuts', 'apple'];
+  const [searchTerm, setSearchTerm] = useState('');
 
   console.log("keywords", keywords);
   useEffect(() => {
@@ -38,12 +46,12 @@ const Search = () => {
 
   const handleSearch = (event) => {
     event.preventDefault();
-    if (name === "") {
+    const keyWordSearch = name + ' ' + selectedIngredients.join(' ');
+    if (keyWordSearch === "") {
       message.warning("Vui lòng điền thông tin tìm kiếm");
       return;
     }
-  
-    navigate(`/search?keywords=${encodeURIComponent(name)}`);
+    navigate(`/search?keywords=${encodeURIComponent(keyWordSearch)}`);
   };
 
   console.log("list_recipes", list_recipes);
@@ -58,6 +66,37 @@ const Search = () => {
     recipes = list_recipes.slice(0, 10);
     setRecipesToShow(10); // Show initial number of recipes
   };
+  const toggleDropdown = () => {
+    setIsOpen(!isOpen);
+  };
+
+  const handleInputChange = (e) => {
+    setSearchTerm(e.target.value.toLowerCase());
+  };
+  const handleIngredientClick = (ingredient) => {
+    if (!selectedIngredients.includes(ingredient)) {
+      setSelectedIngredients([...selectedIngredients, ingredient]);
+    }
+  };
+  const handleDeleteIngredient = (index) => {
+    const updatedIngredients = [...selectedIngredients];
+    updatedIngredients.splice(index, 1);
+    setSelectedIngredients(updatedIngredients);
+  };
+  const renderIngredientList = () => {
+    return items
+      .filter((item) => item.toLowerCase().includes(searchTerm))
+      .map((filteredItem, index) => (
+        <a
+          key={index}
+          href="#"
+          className="block px-4 py-2 text-gray-700 hover:bg-gray-100 active:bg-blue-100 cursor-pointer rounded-md"
+          onClick={() => handleIngredientClick(filteredItem)}
+        >
+          {filteredItem}
+        </a>
+      ));
+  };
 
   return (
     <div className="container border-t mx-auto px-4 py-7">
@@ -67,25 +106,25 @@ const Search = () => {
         </Link>
 
         <div className="relative w-1/2 rounded-lg">
-        <form onSubmit={handleSearch}>
-          <input
-            className="search-input w-full px-4 py-2 border bg-green-500 outline-none placeholder-gray-700 pl-10"
-            style={{ borderRadius: "12px" }}
-            type="text"
-            placeholder="Tìm kiếm công thức..."
-            value={name}
-            onChange={(e) => {
-              setName(e.target.value);
-            }}
-          />
+          <form onSubmit={handleSearch}>
+            <input
+              className="search-input w-full px-4 py-2 border bg-green-500 outline-none placeholder-gray-700 pl-10"
+              style={{ borderRadius: "12px" }}
+              type="text"
+              placeholder="Tìm kiếm công thức..."
+              value={name}
+              onChange={(e) => {
+                setName(e.target.value);
+              }}
+            />
 
-          <button
-            onClick={handleSearch}
-            type="submit"
-            className="absolute top-1/2 right-3 transform -translate-y-1/2"
-          >
-            <img src={loupe} alt="Search" className="w-4 h-4" />
-          </button>
+            <button
+              onClick={handleSearch}
+              type="submit"
+              className="absolute top-1/2 right-3 transform -translate-y-1/2"
+            >
+              <img src={loupe} alt="Search" className="w-4 h-4" />
+            </button>
           </form>
         </div>
 
@@ -97,33 +136,92 @@ const Search = () => {
           />
         </div>
       </nav>
-
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-5 gap-4">
-        {recipes.map((recipe, index) => (
-          <Link to={`/recipes/?ID=${recipe.recipe_id}`} key={recipe.recipe_id}>
-          <div
-            key={index}
-            className="bg-white rounded-lg shadow overflow-hidden transform transition duration-500 hover:scale-105"
+      <div className="flex">
+        <div className="w-1/7 p-7">
+          <button
+            className=" text-white bg-red-600 w-full justify-center rounded-full mb-4 ml-3 mr-3"
+            onClick={handleSearch}
           >
-            <img
-              src={recipe.img_src}
-              alt={recipe.recipe_name}
-              className="w-full h-52 object-cover round-lg"
-            />
-            <div className="p-4">
-              <h3 className="font-bold text-lg">{recipe.recipe_name}</h3>
-              <p className="text-gray-700">{recipe.cook_time}</p>
-              <div className="flex items-center justify-between mt-3">
-                <div>{/* Placeholder for icons and other details */}</div>
-                <div className="flex items-center">
-                  <img src={starIcon} alt="Star" className="h-5 w-5 mr-2" />
-                  <span className="font-bold">{recipe.rating}</span>
+            Filters
+          </button>
+          <div className="min-h-screen flex justify-center">
+            <div className="relative group">
+              <div className="selectedIngredients mb-4">
+                <ul>
+                  {selectedIngredients.map((ingredient, index) => (
+                    <li key={index} className="flex items-center">
+                      <img
+                        src={trashCan}
+                        alt="TrashCan"
+                        className="h-4 w-4 mr-2 cursor-pointer"
+                        onClick={() => handleDeleteIngredient(index)}
+                      />
+                      {ingredient}
+                    </li>
+                  ))}
+                </ul>
+              </div>
+              <button
+                id="dropdown-button"
+                className="inline-flex justify-center w-full px-4 py-2 text-sm font-medium text-gray-700 bg-white border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-blue-500 ml-3 mr-3"
+                onClick={toggleDropdown}
+              >
+                <span className="mr-2">Ingredients</span>
+                <svg xmlns="http://www.w3.org/2000/svg" className="w-5 h-5 ml-2 -mr-1" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                  <path fillRule="evenodd" d="M6.293 9.293a1 1 0 011.414 0L10 11.586l2.293-2.293a1 1 0 111.414 1.414l-3 3a1 1 0 01-1.414 0l-3-3a1 1 0 010-1.414z" clipRule="evenodd" />
+                </svg>
+              </button>
+              <div
+                id="dropdown-menu"
+                className={`absolute right-0 mt-2 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 p-1 space-y-1 ${isOpen ? '' : 'hidden'}`}
+              >
+                <input
+                  id="search-input"
+                  className="block w-full px-4 py-2 text-gray-800 border rounded-md  border-gray-300 focus:outline-none"
+                  type="text"
+                  placeholder="Search items"
+                  autoComplete="off"
+                  value={searchTerm}
+                  onChange={handleInputChange}
+                />
+                <div className="scrollable-menu max-h-48 overflow-y-auto">
+                  {renderIngredientList()}
                 </div>
               </div>
+
             </div>
+
           </div>
-        </Link>
-        ))}
+        </div>
+        <div className="w-6/7">
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-5 gap-4">
+            {recipes.map((recipe, index) => (
+              <Link to={`/recipes/?ID=${recipe.recipe_id}`} key={recipe.recipe_id}>
+                <div
+                  key={index}
+                  className="bg-white rounded-lg shadow overflow-hidden transform transition duration-500 hover:scale-105"
+                >
+                  <img
+                    src={recipe.img_src}
+                    alt={recipe.recipe_name}
+                    className="w-full h-52 object-cover round-lg"
+                  />
+                  <div className="p-4">
+                    <h3 className="font-bold text-lg h-16">{recipe.recipe_name}</h3>
+                    <p className="text-gray-700">{recipe.cook_time}</p>
+                    <div className="flex items-center justify-between mt-3">
+                      <div>{/* Placeholder for icons and other details */}</div>
+                      <div className="flex items-center">
+                        <img src={starIcon} alt="Star" className="h-5 w-5 mr-2" />
+                        <span className="font-bold">{recipe.rating}</span>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </Link>
+            ))}
+          </div>
+        </div>
       </div>
       <div className="text-center mt-8">
         {recipesToShow > initialRecipesToShow ? (
