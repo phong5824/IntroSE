a3db2878871f2f9b4032e3857ada3a99647f0b4e
Pake <100368114+TTH-Pake@users.noreply.github.com>
2023-12-09 00:24:50 +0700

    clean

---
+++ client/src/assets/index.jsx
@@ -0,0 +1,8 @@
+import Clock from "./clock.png";
+import chatIcon from "./chat.png";
+import avatarIcon from "./avatar.png";
+import SendIcon from "./send.png";
+import Bookmark from "./bookmark.png";
+import Share from "./share.png";
+
+export { Clock, chatIcon, avatarIcon, SendIcon, Bookmark, Share}
