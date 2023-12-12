29f63b9a274021183825394a58af1834e5131d46
Phong ne <phog.27.12@gmail.com>
2023-12-12 22:34:59 +0700

    Adjust picture in login/register/reset password

--- client/src/components/views/resetPassword.jsx
+++ client/src/components/views/resetPassword.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import { handleResetPassword } from "../../action/accountAction";
 import { useNavigate } from "react-router-dom";
 import Logo from "../../assets/logo-recipe.png";
-import {message} from "antd";
+import { message } from "antd";
 
 export default function ResetPassword() {
   const [email, setEmail] = useState("");
@@ -23,12 +23,12 @@ export default function ResetPassword() {
       return;
     }
 
-    if(password.length< 6){
+    if (password.length < 6) {
       message.error("Password must be at least 6 characters");
       return;
     }
 
-    if(password !== userData.verifyPassword){
+    if (password !== userData.verifyPassword) {
       message.error("Password must be match");
       return;
     }
@@ -44,16 +44,15 @@ export default function ResetPassword() {
   };
 
   return (
-    <div className="flex items-center justify-center h-screen bg-green-400">
-      <div className="login-container flex relative flex-col items-center justify-around w-[500px] h-[410px] bg-green-300 rounded-3xl">
-        <div
-          className="login-logo w-24 h-24 rounded-full mb-[-4rem]"
-          style={{
-            backgroundImage: `url(${Logo})`,
-            backgroundRepeat: "no-repeat",
-            backgroundSize: "cover",
-          }}
-        ></div>
+    <div className="flex items-center justify-center h-full bg-green-400">
+      <div className="login-container flex relative flex-col items-center justify-around w-[500px] h-auto bg-green-300 rounded-3xl p-4">
+
+        <img
+          className="w-24 h-24 rounded-full object-cover"
+          src={Logo}
+          alt="Logo"
+        />
+
         <div className="login-form w-[350px] py-1">
           <form
             className="flex flex-col justify-around w-full"
@@ -85,7 +84,7 @@ export default function ResetPassword() {
             />
 
             <div className="bg-green-300 text-black text-lg font-semibold text-left p-2 px-4 my-1 mx-auto w-[max-content]">
-            Verify Password
+              Verify Password
             </div>
             <input
               type="password"
@@ -97,9 +96,9 @@ export default function ResetPassword() {
               placeholder="type here..."
             />
             <button className="btn w-[50%] py-2 rounded-full mt-4 text-black text-base text-center cursor-pointer mx-auto bg-red-400 hover:bg-red-500 hover:font-semibold hover:shadow-lg transition duration-300"
-            onSubmit={onSubmit}>
-            Change Password
-             
+              onSubmit={onSubmit}>
+              Change Password
+
             </button>
           </form>
         </div>
