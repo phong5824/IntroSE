09049a1b43828becca2fbdace3b4869846890a1c
Phong ne <phog.27.12@gmail.com>
2023-12-14 21:40:01 +0700

    Update blog ... npm install moment

--- client/src/components/views/Blog.jsx
+++ client/src/components/views/Blog.jsx
@@ -23,7 +23,7 @@ const Blog = () => {
   }, []);
 
   return (
-    <>
+    <div className="bg-green-200">
       <Navbar />
       <div className="bg-green-200 py-8">
         <h1 className="text-2xl font-bold mb-8 text-center">
@@ -34,11 +34,14 @@ const Blog = () => {
             <BlogPostCard key={post.id} post={post} />
           ))}
         </div>
+        <div className="container mx-auto text-center mt-4">
+          <Pagination defaultCurrent={1} total={500} />
+        </div>
       </div>
-      <Pagination defaultCurrent={1} total={500} />;
       <Footer />
-    </>
+    </div>
   );
+
 };
 
 export default Blog;
