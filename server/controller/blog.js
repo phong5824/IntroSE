e2fc9e0fa9eaef0ba369514b78023a8cfaccb077
Tan-Hiep To <100368114+TanHiep-To@users.noreply.github.com>
2023-12-16 17:01:22 +0700

    Phongne (#42)
    * Update UI Blog
    
    * Update UI Blog
    
    * update blog
    
    * update Blogs
    
    * update recipeDetail
    
    * Update blog ... npm install moment
    
    * Update blog ... npm install moment
    
    * Update chatbot call API Chatgpt
    
    * change API key
    
    ---------
    
    Co-authored-by: Pake <100368114+TTH-Pake@users.noreply.github.com>
    Co-authored-by: Phong ne <phog.27.12@gmail.com>

--- server/controller/blog.js
+++ server/controller/blog.js
@@ -7,11 +7,8 @@ const getAllBlogs = async (req, res) => {
     try {
         const blogs = await Blogs.find({});
 
-        for (const blog of blogs)
-        {
-            blog.created_time = blog.created_time.getDate();
-        }
-        res.status(200).json({success:true,blogs});
+        res.status(200).json({ success: true, blogs });
+
     } catch (error) {
         res.status(500).json({ message: error.message });
     }
--- server/server.js
+++ server/server.js
@@ -11,6 +11,8 @@ const recipesRouter = require("./routes/recipes");
 const ingredientRouter = require("./routes/ingredient.js");
 const commentRouter = require("./routes/comment.js");
 const blogRouter = require("./routes/blog.js");
+
+const chatbotRouter = require("./routes/chatbot.js");
 const db = require("./db/index");
 
 const app = express();
@@ -19,7 +21,11 @@ app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 
+
+app.use("/blog", blogRouter);
+app.use("/chatbot", chatbotRouter);
 app.use("/",blogRouter);
+
 app.use("/", accountRouter);
 app.use("/", recipesRouter);
 app.use("/users", userRouter);
@@ -32,4 +38,3 @@ db.on("error", (stream) => {
 app.listen(8000, () => {
   console.log(`Node API app is running on port 8000`);
 });
-// app.use('/',accountModel)
