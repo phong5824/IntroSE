a4e84a1a762a03b7f117993a60897c10b35488d1
Duy Khang <duykhang2103@gmail.com>
2024-01-03 23:56:33 +0700

    merge main into k

--- src/server/routes/chatbot.js
+++ src/server/routes/chatbot.js
@@ -1,21 +1,21 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const chatbotController = require('../controller/chatbot');
+const chatbotController = require("../controller/chatbot");
 
 router.use(express.json());
 
-router.post('/', async (req, res) => {
-    try {
-        console.log('Received messages:', req.body.messages);
+router.post("/", async (req, res) => {
+  try {
+    console.log("Received messages:", req.body.messages);
 
-        const reply = await chatbotController.askChatbot(req.body.messages);
+    const reply = await chatbotController.askChatbot(req.body.messages);
 
-        console.log('Response from askChatbot:', reply);
+    console.log("Response from askChatbot:", reply);
 
-        res.json({ reply });
-    } catch (error) {
-        res.status(500).json({ error: 'Internal Server Error' });
-    }
+    res.json({ reply });
+  } catch (error) {
+    res.status(500).json({ error: "Internal Server Error" });
+  }
 });
 
 module.exports = router;
